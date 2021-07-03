0863117610c4f62c3c4fc89aa56e29cf0f59ce59
blue157751 <victorgabriel6090@gmail.com>
2021-07-03 18:49:34 -0300

    calculo que te dá varios possiveis resultados de uma vez.

---
+++ ex005.py
@@ -0,0 +1,11 @@
+print('nos dê dois números e mostraremos diversos calculos que podem ser feitos com eles:')
+n1 = int(input('escolha um número'))
+n2 = int(input('escolha outro número'))
+
+print ('{} + {} é: {}!'.format (n1, n2, n1+n2))
+print ('{} - {} é: {}!'.format (n1, n2, n1-n2))
+print ('{} / {} é: {}!'.format (n1, n2 ,n1/n2))
+print ('{} * {} é: {}!'.format (n1, n2, n1*n2))
+
+
+
