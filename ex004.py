a9cc85a3855d375284001dc66a5c6b86c052104b
blue157751 <victorgabriel6090@gmail.com>
2021-07-03 18:31:39 -0300

    adicionando uma forma criativa de calcular

---
+++ ex004.py
@@ -0,0 +1,14 @@
+ordem de precedência: 'matematica'
+1 resolver ()
+2** numero elevado
+3 * / // % o que vier primeiro você resolve
+4 + -
+
+exemplos:
+5+3*2==11
+
+3*5+4**2==31
+
+3x(5+4)**2==243
+
+
